c73f3622372467ae936e2605e54bad7a75b3deab
reynan-dev <vieira@reynan.dev>
2022-11-16 20:57:49 +0100

    feat: updating validate-branch-name

--- .validate-branch-namerc.js
+++ .validate-branch-namerc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    pattern: '^(main|staging|production)$|^(jk|rv|nc)/^(bump|feat|fix|rel(?:ease)?)/.+$',
+    pattern: '^(main|staging|production)$|^(jk|rv|nc)$/^(refactor|feat|fix|docs|perf|test|build|ci|chore|revert)/.+$',
     errorMsg:
         '🤨 La branche que tu essaies de pusher ne respecte pas nos conventions, tu peux la renommer avec `git branch -m <nom-actuel> <nouveau-nom>`',
 }
