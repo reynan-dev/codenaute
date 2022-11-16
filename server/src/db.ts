d5d7fa5fe73337d3b337ee36010bade23d23cf38
reynan-dev <vieira@reynan.dev>
2022-11-16 07:46:51 +0100

    feat(server): creting db connecting

---
+++ server/src/db.ts
@@ -0,0 +1,30 @@
+import { DataSource } from "typeorm";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+import Common from "./models/Common.js";
+
+const dataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ["src/entity/**/*.js"],
+  synchronize: true,
+  logging: true,
+});
+
+const connectDB = async function () {
+  try {
+    await dataSource.initialize();
+    console.log("Successfully connected to database");
+  } catch (error) {
+    console.log("Database connection error");
+    console.log(error);
+  }
+};
+
+export { DataSource, connectDB };
