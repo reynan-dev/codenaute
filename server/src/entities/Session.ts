324b93304284db4625e6fc3ef7950640f7d79ba5
reynan-dev <vieira@reynan.dev>
2022-11-17 17:41:40 +0100

    feat(server): create entities to authentication

---
+++ server/src/entities/Session.ts
@@ -0,0 +1,44 @@
+import { randomBytes } from 'crypto';
+import { Field, ObjectType } from 'type-graphql';
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import Member from './Member.js';
+
+@Entity()
+@ObjectType()
+export default class Session {
+  constructor(member: Member) {
+    this.member = member;
+  }
+  @PrimaryColumn('varchar', { length: 32 })
+  token: string;
+
+  @ManyToOne(() => Member, { eager: true })
+  @Field()
+  member: Member;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @Column('timestamp with time zone')
+  expiresAt: Date;
+
+  @BeforeInsert()
+  expirateIn() {
+    const now = new Date();
+    this.expiresAt = now.setHours(now.getDay() + 7);
+  }
+
+  @BeforeInsert()
+  setId() {
+    this.token = randomBytes(16).toString('hex');
+  }
+}
