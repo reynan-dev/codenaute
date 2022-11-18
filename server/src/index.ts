e69a1c94ea22886bac7db138c28f2e0922339df0
Reynan VIEIRA <100930729+reynan-dev@users.noreply.github.com>
2022-11-18 10:23:56 +0100

    feat(server): setup server side and setting docker compose (#4)
    ## Ticket
    
    **Title:** ETQ Dev, j’ai le setup de dépendances côté back
    **Notion Link:**
    https://www.notion.so/ETQ-Dev-j-ai-le-setup-de-d-pendances-c-t-back-f4f3fffc02854524b41f6b1a73435e2f
    
    ## Description
    
    - Setup server architecture in src folder, with sub-folders: migrations,
    entities, resolvers and services.
    - Setup tests architecture in tests folder
    - Setup Dockerfile to server
    - Update Dockerfile to database (include adminer and redis)
    - Update docker-compose in route folder. 
    - Create generic config to server in apollo server with typeorm.
    - Create first and generic migration to database.
    
    ## Guidelines
    
    🔗 Link your PRs to their related Notion tickets
    ✍️ Use the commit convention to your pull request tittle
    📃 If your code affects the build, update `README.md`
    🚫 Do not merge a PR until all comments are resolved
    🗑 Remove your branch after merging

---
+++ server/src/index.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { buildSchema } from 'type-graphql';
+
+import CommonResolver from './resolvers/CommonResolver.js';
+
+import { connectDB } from './db.js';
+
+const startServer = async () => {
+  await connectDB();
+
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [CommonResolver],
+    }),
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
+  });
+
+  const { url } = await server.listen(process.env.GRAPHQL_PORT);
+
+  console.log(`🚀 Server ready at ${url}`);
+};
+startServer();
