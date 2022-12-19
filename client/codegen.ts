28bbd644f3f7d97a7ab05e9a5c678d7ccca5c4e5
NeelClaudel <92921200+NeelClaudel@users.noreply.github.com>
2022-12-19 17:48:56 +0100

    chore(front): setup client (#8)
    ## Ticket
    
    **Title:**
    **Notion Link:**
    
    ## Description
    
    ## Guidelines
    
    🔗 Link your PRs to their related Notion tickets
    ✍️ Use the commit convention to your pull request tittle
    📃 If your code affects the build, update `README.md`
    🚫 Do not merge a PR until all comments are resolved
    🗑 Remove your branch after merging
    
    Co-authored-by: reynan-dev <vieira@reynan.dev>
    Co-authored-by: jkergal <hello@johannkergal.fr>

---
+++ client/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:4000',
+  documents: 'src/**/*.tsx',
+  generates: {
+    'src/gql': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+};
+
+export default config;
---
+++ client/src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/',
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement,
+);
+root.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
+  </React.StrictMode>,
+);
+
+reportWebVitals();
