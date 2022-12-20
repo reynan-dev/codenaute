a18d8badc87c9916485226965109451f759af2ed
reynan-dev <vieira@reynan.dev>
2022-12-20 15:07:03 +0100

    chore: removing common stuffs

--- server/src/resolvers/args/CommonResolver.ts
+++
@@ -1,9 +0,0 @@
-import { Args, Mutation, Query, Resolver } from 'type-graphql';
-
-@Resolver()
-export default class CommonResolver {
-	@Query(() => String)
-	async hello() {
-		return 'Hello World!';
-	}
-}
