03642485146b5428c47ab1348ae7eec464f2cbe0
reynan-dev <vieira@reynan.dev>
2022-12-21 15:53:12 +0100

    chore: removing old jest generic test

--- server/src/db.ts
+++ server/src/db.ts
@@ -9,19 +9,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const DB_PORT = process.env.DB_PORT as number | undefined;
+const DB_TEST_PORT = process.env.DB_TEST_PORT as number | undefined;
 
 const dataSource = new DataSource({
 	type: 'postgres',
-	host: process.env.DB_HOST,
-	port: DB_PORT,
-	username: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	entities: [`${__dirname}/**/entities/*.{ts, js}`],
-	migrations: [`${__dirname}/**/migrations/*.{ts, js}`]
+	host: process.env.NODE_ENV === 'test' ? process.env.DB_TEST_HOST : process.env.DB_HOST,
+	port: process.env.NODE_ENV === 'test' ? DB_TEST_PORT : DB_PORT,
+	username: process.env.NODE_ENV === 'test' ? process.env.DB_TEST_USER : process.env.DB_USER,
+	password:
+		process.env.NODE_ENV === 'test' ? process.env.DB_TEST_PASSWORD : process.env.DB_PASSWORD,
+	database:
+		process.env.NODE_ENV === 'test' ? process.env.DB_TEST_DATABASE : process.env.DB_DATABASE,
+	entities: [`${__dirname}/**/entities/*.${process.env.NODE_ENV === 'test' ? 'ts' : 'js'}`],
+	migrations: [`${__dirname}/**/migrations/*.${process.env.NODE_ENV === 'test' ? 'ts' : 'js'}`],
+	logging: process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error']
 });
 
-const connectDB = async function () {
+const startDatabase = async function () {
 	try {
 		await dataSource.initialize();
 		console.log('🎉 Successfully connected to database');
@@ -31,4 +35,14 @@ const connectDB = async function () {
 	}
 };
 
-export { dataSource, connectDB };
+const closeDatabase = async function () {
+	try {
+		await dataSource.close();
+		console.log('💀 Successfully closed database connection');
+	} catch (error) {
+		console.log('😞 Database disconnection error');
+		console.log(error);
+	}
+};
+
+export { dataSource, startDatabase, closeDatabase };
