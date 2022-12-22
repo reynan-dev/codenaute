9b70db86394708fb2f1502a2de3eb621ed5ed6c0
reynan-dev <vieira@reynan.dev>
2022-12-22 18:03:13 +0100

    test(server): removing garbage

--- server/tests/services/base/find.test.ts
+++
@@ -1,7 +0,0 @@
-import BaseServices from '../../../src/services/base/BaseServices';
-
-describe.skip('Find method in Base Services', () => {
-  it('should be a function', () => {
-    expect(BaseServices.find()).toBeInstanceOf(Function);
-  });
-});
