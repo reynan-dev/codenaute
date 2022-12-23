168d01649ea6ae07e34aab215b7073369989919a
reynan-dev <vieira@reynan.dev>
2022-12-23 14:47:37 +0100

    chore(prettierignore): including coverage test report ignore

---
+++ server/tests/integrations/BaseServices/find.integration.test.ts
@@ -0,0 +1,34 @@
+import { closeDatabase, dataSource, startDatabase } from '../../../src/db';
+import BaseService from '../../../src/services/base/BaseServices';
+import MemberServices from '../../../src/services/MemberServices';
+
+describe('BaseService.find', () => {
+	beforeAll(async () => {
+		await startDatabase();
+	});
+
+	afterAll(async () => {
+		await closeDatabase();
+	});
+
+	beforeEach(async () => {
+		for (const entity of dataSource.entityMetadatas) {
+			const repository = dataSource.getRepository(entity.name);
+			await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+		}
+	});
+
+	describe('when no records exist', () => {
+		it('returns an empty array', async () => {
+			expect(await MemberServices.find()).toEqual([]);
+		});
+	});
+
+	describe('when records exist', () => {
+		it('returns an array of records', async () => {
+			const member = await MemberServices.signUp('usertest', 'user@test.com', 'password');
+
+			expect(await MemberServices.find()).toEqual([member]);
+		});
+	});
+});
