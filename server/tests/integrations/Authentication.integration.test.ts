3e7fa459f316c11ed3e8dccbbeafd0481a57ac92
reynan-dev <vieira@reynan.dev>
2022-12-23 12:05:02 +0100

    style: making prettier-fix

--- server/tests/integrations/Authentication.integration.test.ts
+++ server/tests/integrations/Authentication.integration.test.ts
@@ -5,63 +5,63 @@ import { dataSource, closeDatabase, startDatabase } from '../../src/db';
 import { INVALID_CREDENTIALS_ERROR_MESSAGE } from '../../src/utils/errorMessage';
 
 describe('Authentication integration test', () => {
-  beforeAll(async () => {
-    await startDatabase();
-  });
+	beforeAll(async () => {
+		await startDatabase();
+	});
 
-  afterAll(async () => {
-    await closeDatabase();
-  });
+	afterAll(async () => {
+		await closeDatabase();
+	});
 
-  beforeEach(async () => {
-    for (const entity of dataSource.entityMetadatas) {
-      const repository = dataSource.getRepository(entity.name);
-      await repository.query(
-        `TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`
-        );
-    }
-  });
+	beforeEach(async () => {
+		for (const entity of dataSource.entityMetadatas) {
+			const repository = dataSource.getRepository(entity.name);
+			await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+		}
+	});
 
-  describe("when email address doesn't belong to existing user", () => {
-    it ("throws invalid credentials error", async () => {
-      const email = 'unknown@email.com';
+	describe("when email address doesn't belong to existing user", () => {
+		it('throws invalid credentials error', async () => {
+			const email = 'unknown@email.com';
 
-      expect(() => MemberServices.signIn(email, 'password')).rejects.toThrowError(INVALID_CREDENTIALS_ERROR_MESSAGE)
-    });
-  });
+			expect(() => MemberServices.signIn(email, 'password')).rejects.toThrowError(
+				INVALID_CREDENTIALS_ERROR_MESSAGE
+			);
+		});
+	});
 
+	describe('when email address belongs to existing user', () => {
+		const email = 'email@test.com';
+		describe('when password is invalid', () => {
+			it('throws invalid credentials error', async () => {
+				await MemberServices.signUp('username', email, 'password');
 
-  describe("when email address belongs to existing user", () => {
-    const email = 'email@test.com'
-    describe("when password is invalid", () => {
-      it ("throws invalid credentials error", async () => {
-        await MemberServices.signUp('username', email, 'password');
+				expect(() => MemberServices.signIn(email, 'incorrect')).rejects.toThrowError(
+					INVALID_CREDENTIALS_ERROR_MESSAGE
+				);
+			});
+		});
 
-        expect(() => MemberServices.signIn(email, 'incorrect')).rejects.toThrowError(INVALID_CREDENTIALS_ERROR_MESSAGE)
-      });
-    });
+		describe('when password is valid', () => {
+			it('creates session in database', async () => {
+				await MemberServices.signUp('username', email, 'password');
 
-    describe("when password is valid", () => {
-      it ("creates session in database", async () => {
-        await MemberServices.signUp('username', email, 'password');
+				const { session } = await MemberServices.signIn(email, 'password');
 
-        const { session } = await MemberServices.signIn(email, 'password');
+				const sessions = await SessionServices.findByToken(session.token);
 
-        const sessions = await SessionServices.findByToken(session.token);
+				expect(sessions?.member.email).toEqual(email);
+			});
 
-        expect(sessions?.member.email).toEqual(email);
-      });
+			it('returns user and session', async () => {
+				await MemberServices.signUp('username', email, 'password');
 
-      it ("returns user and session", async () => {
-        await MemberServices.signUp('username', email, 'password');
+				const login = await MemberServices.signIn(email, 'password');
 
-        const login = await MemberServices.signIn(email, 'password');
-
-        expect(login).toHaveProperty('user');
-        expect(login).toHaveProperty('session');
-        expect(login.user.email).toEqual(email);
-      });
-    });
-  });
+				expect(login).toHaveProperty('user');
+				expect(login).toHaveProperty('session');
+				expect(login.user.email).toEqual(email);
+			});
+		});
+	});
 });
-
