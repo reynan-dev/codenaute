eea1507b6858a2d74d2359a1304245cc43b73b95
reynan-dev <vieira@reynan.dev>
2022-12-24 15:13:32 +0100

    test(server): including integration tests

---
+++ server/tests/integrations/services/RoutingTokensServices/create.integration.test.ts
@@ -0,0 +1,35 @@
+import MemberServices from '../../../../src/services/MemberServices';
+import RoutingTokenServices from '../../../../src/services/RoutingTokenServices';
+
+import { dataSource, closeDatabase, startDatabase } from '../../../../src/db';
+
+describe('Create RoutingToken integration test', () => {
+	beforeAll(async () => {
+		await startDatabase();
+	});
+
+	afterAll(async () => {
+		await closeDatabase();
+	});
+
+	beforeEach(async () => {
+		for (const entity of dataSource.entityMetadatas) {
+			const repository = dataSource.getRepository(entity.name);
+			await repository.query(`TRUNCATE ${entity.tableName} RESTART IDENTITY CASCADE;`);
+		}
+	});
+
+	describe('when create routing token', () => {
+		it('return an member', async () => {
+			const username = 'username';
+			const email = 'unknown@email.com';
+			const password = 'password';
+
+			const member = await MemberServices.signUp(username, email, password);
+
+			const routingToken = await RoutingTokenServices.create(member.id);
+
+			expect(await RoutingTokenServices.findByToken(routingToken.token)).toEqual(routingToken);
+		});
+	});
+});
