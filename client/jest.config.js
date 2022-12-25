be05da061f67fbfa28c336cf4a2245fb15e599f7
reynan-dev <vieira@reynan.dev>
2022-12-25 15:06:21 +0100

    test(client): updating jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -19,5 +19,5 @@ exports.default = {
 	// The test environment that will be used for testing
 	testEnvironment: 'jest-environment-node',
 	// A list of paths to modules that run some code to configure or set up the testing framework before each test
-	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts']
+	setupFilesAfterEnv: ['@testing-library/jest-dom']
 };
