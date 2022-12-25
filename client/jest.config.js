cba4111dd3a776b9a9c380790865454c8e5de613
reynan-dev <vieira@reynan.dev>
2022-12-25 14:59:41 +0100

    test(client): create jest config in client side

---
+++ client/jest.config.js
@@ -0,0 +1,23 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.default = {
+	// Automatically clear mock calls, instances, contexts and results before every test
+	clearMocks: true,
+	// Indicates whether the coverage information should be collected while executing the test
+	collectCoverage: true,
+	// The directory where Jest should output its coverage files
+	coverageDirectory: './tests/coverage',
+	// An array of regexp pattern strings used to skip coverage collection
+	coveragePathIgnorePatterns: ['/node_modules/', '/src/migrations/'],
+	// Indicates which provider should be used to instrument code for coverage
+	coverageProvider: 'v8',
+	// A preset that is used as a base for Jest's configuration
+	preset: 'ts-jest',
+	// The test environment that will be used for testing
+	testEnvironment: 'jest-environment-node',
+	// A list of paths to modules that run some code to configure or set up the testing framework before each test
+	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts']
+};
