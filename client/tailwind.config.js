3676052602c0abe8a2a6f4b22267a11575500f53
jkergal <hello@johannkergal.fr>
2023-01-23 12:41:32 +0100

    chore(tailwind): add colors in setup file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#44A5FE'
+				},
+				secondary: {
+					DEFAULT: '#FAD402'
+				},
+				success: {
+					DEFAULT: '#28AF65'
+				},
+				warning: {
+					DEFAULT: '#F1DB4B'
+				},
+				danger: {
+					DEFAULT: '#F76958'
+				},
+				black: {
+					DEFAULT: '#0C0D10'
+				},
+				white: {
+					DEFAULT: '#F2F2F2'
+				},
+				dark: {
+					DEFAULT: '#131416',
+					300: '#A0A4AD',
+					600: '#35373E',
+					700: '#27292E',
+					800: '#202225',
+					900: '#18191B'
+				}
+			}
+		}
 	},
 	plugins: []
 };
