7b68ea51a0530fa58964861e67f1210b0d0243c9
Reynan VIEIRA <100930729+reynan-dev@users.noreply.github.com>
2023-01-25 11:07:00 -0300

    chore: updating packages and pnpm (#22)
    ## Ticket
    
    **Title:** ETQ Dev, j’ai des packages updated
    **Notion Link:**
    https://www.notion.so/jkergal/ETQ-Dev-j-ai-des-packages-updated-926d7c9d6fa24bba9bd958948512b5ee
    
    ## Description
    
    - Updated pnpm
    - Updated packages in the root, server, and client folders
    - Created jest.config on the client side
    - Created a tests folder on the client side and moved the test to
    
    ## Guidelines
    
    🔗 Link your PRs to their related Notion tickets
    ✍️ Use the commit convention to your pull request title
    📃 If your code affects the build, update `README.md`
    🚫 Do not merge a PR until all comments are resolved
    🗑 Remove your branch after merging

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 import '@testing-library/jest-dom/extend-expect';
 
 test('renders learn react link', () => {
