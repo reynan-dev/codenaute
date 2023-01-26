d764914c9197fc8f36c8714d528140af119d693e
Johann Kergal <89657288+jkergal@users.noreply.github.com>
2023-01-26 01:52:40 +0100

    chore(client): finish setup + create important generic components (#24)
    ## Ticket
    
    **Title:**
    **Notion Link:**
    
    ## Description
    
    ![image](https://user-images.githubusercontent.com/89657288/214707788-fe6fa282-8ec4-40dd-8421-1a50d5bf6e1c.png)
    
    ## Guidelines
    
    🔗 Link your PRs to their related Notion tickets
    ✍️ Use the commit convention to your pull request title
    📃 If your code affects the build, update `README.md`
    🚫 Do not merge a PR until all comments are resolved
    🗑 Remove your branch after merging

---
+++ client/src/components/button/Button.tsx
@@ -0,0 +1,100 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, MouseEventHandler, useMemo } from 'react';
+import { BiLoaderAlt } from 'react-icons/bi';
+import { clsxMerge } from '../../helpers/clsxMerge';
+
+type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
+	children: React.ReactNode;
+	isLoading?: boolean;
+	size?: 'medium' | 'small';
+} & (
+		| {
+				designType?: 'primary';
+				disabled?: boolean;
+		  }
+		| { designType?: 'secondary' | 'text'; disabled?: never }
+	) &
+	(
+		| {
+				type: 'submit';
+				linkUrl?: never;
+				onClick?: never;
+		  }
+		| {
+				onClick?: never;
+				linkUrl: HTMLLinkElement['href'] | undefined;
+		  }
+		| {
+				onClick: MouseEventHandler<HTMLButtonElement>;
+				linkUrl?: never;
+		  }
+	);
+
+export const Button: React.FC<ButtonProps> = ({
+	children,
+	className,
+	isLoading,
+	disabled,
+	designType = 'primary',
+	size = 'medium',
+	linkUrl,
+	...props
+}) => {
+	const style = useMemo(() => {
+		const primaryStyle = clsxMerge(
+			'relative inline-flex min-w-56 items-center justify-center',
+			'border-primary bg-primary rounded-full border-2 border-solid font-semibold text-black text-xl',
+			'transition ease-in-out duration-150',
+			'hover:bg-primary-100 hover:border-primary-100',
+			'disabled:border-dark-300 disabled:bg-dark-300',
+			isLoading && 'disabled:border-primary-200 disabled:bg-primary-200',
+			size === 'small' ? 'py-2 px-4 text-lg' : 'py-4 px-4 text-xl'
+		);
+		if (designType === 'primary') return primaryStyle;
+		const secondaryStyle = clsxMerge(
+			primaryStyle,
+			'bg-transparent text-primary border-primary',
+			'hover:bg-primary hover:border-primary hover:text-black',
+			isLoading && 'disabled:bg-primary-200 disabled:border-primary-200 disabled:text-white'
+		);
+		return secondaryStyle;
+	}, [designType, isLoading, size]);
+
+	const renderLoader = useMemo(() => {
+		if (!isLoading) return;
+		return (
+			<div className='absolute right-4 inline-flex animate-spin items-center self-center'>
+				<BiLoaderAlt />
+			</div>
+		);
+	}, [isLoading]);
+
+	const isDisabled = useMemo(
+		() => isLoading || (disabled && designType === 'primary'),
+		[isLoading, disabled, designType]
+	);
+
+	if (linkUrl !== undefined) {
+		return (
+			<link href={linkUrl} className={clsxMerge(style, className)}>
+				<span className='truncate'>{children}</span>
+			</link>
+		);
+	}
+
+	if (designType === 'text') {
+		return (
+			<button {...props} className='font-semibold text-primary'>
+				{children}
+			</button>
+		);
+	}
+
+	return (
+		<>
+			<button {...props} className={clsxMerge(style, className)} disabled={isDisabled}>
+				<span className='truncate'>{children}</span>
+				{renderLoader}
+			</button>
+		</>
+	);
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,55 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: '#44A5FE',
+					100: '#7ec0fc'
+				},
+				secondary: {
+					DEFAULT: '#8B77E2'
+				},
+				success: {
+					DEFAULT: '#28AF65'
+				},
+				warning: {
+					DEFAULT: '#F1DB4B'
+				},
+				danger: {
+					DEFAULT: '#F76958'
+				},
+				black: {
+					DEFAULT: '#0C0D10'
+				},
+				white: {
+					DEFAULT: '#F2F2F2'
+				},
+				dark: {
+					DEFAULT: '#131416',
+					300: '#A0A4AD',
+					500: '#545861',
+					600: '#35373E',
+					700: '#27292E',
+					800: '#202225',
+					900: '#18191B'
+				}
+			},
+			fontFamily: {
+				archivo: ['Archivo']
+			},
+			minWidth: {
+				56: '14rem'
+			}
+		}
 	},
-	plugins: []
+	plugins: [
+		require('tailwindcss'),
+		require('autoprefixer'),
+		plugin(require('./src/styles/plugins/colorVarsExtractor'))
+	]
 };
