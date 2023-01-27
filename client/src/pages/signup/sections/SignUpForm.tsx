48f147ac54992450ac7677543d0bc923a6d47d0c
jkergal <hello@johannkergal.fr>
2023-01-27 09:44:34 +0100

    feat(client): improve signup page layout

--- client/src/pages/signup/sections/SignUpForm.tsx
+++ client/src/pages/signup/sections/SignUpForm.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Button } from '../../../components/Button/Button';
 import { Input } from '../../../components/Input/Input';
 
@@ -9,19 +8,16 @@ interface SignUpFormProps {
 export default function SignUpForm({ className }: SignUpFormProps) {
 	return (
 		<div className={className}>
-			<Input placeholder='SuperCoder' label='Username' className='py-4' />
-			<Input placeholder='john@doe.com' label='Email' className='py-4' />
-			<Input placeholder='Password' label='Password' type='password' className='py-4' />
-			<Input
-				placeholder='Repeat password'
-				label='Repeat password'
-				type='password'
-				className='py-4'
-			/>
+			<form className='full-center-col w-full space-y-8'>
+				<Input placeholder='SuperCoder' label='Username' />
+				<Input placeholder='john@doe.com' label='Email' />
+				<Input placeholder='Password' label='Password' type='password' />
+				<Input placeholder='Repeat password' label='Repeat password' type='password' />
 
-			<Button onClick={() => alert('click')} isLoading className='my-8'>
-				Se connecter
-			</Button>
+				<Button onClick={() => alert('click')} isLoading className='my-8'>
+					Se connecter
+				</Button>
+			</form>
 		</div>
 	);
 }
