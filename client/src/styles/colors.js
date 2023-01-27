9bb72e42ccd3d39223e0a6c742e0e6fde34581b5
jkergal <hello@johannkergal.fr>
2023-01-27 11:18:25 +0100

    refactor(client): create colors constants file for a single source of truth

---
+++ client/src/styles/colors.js
@@ -0,0 +1,35 @@
+const COLORS = {
+	PRIMARY: {
+		DEFAULT: '#44A5FE',
+		100: '#7ec0fc'
+	},
+	SECONDARY: {
+		DEFAULT: '#8B77E2'
+	},
+	SUCCESS: {
+		DEFAULT: '#28AF65'
+	},
+	WARNING: {
+		DEFAULT: '#F1DB4B'
+	},
+	DANGER: {
+		DEFAULT: '#F76958'
+	},
+	BLACK: {
+		DEFAULT: '#0C0D10'
+	},
+	WHITE: {
+		DEFAULT: '#F2F2F2'
+	},
+	DARK: {
+		DEFAULT: '#131416',
+		300: '#A0A4AD',
+		500: '#545861',
+		600: '#35373E',
+		700: '#27292E',
+		800: '#202225',
+		900: '#18191B'
+	}
+};
+
+module.exports = COLORS;
