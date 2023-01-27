def164911595c75336b995a70f5c82532ab6ff2b
jkergal <hello@johannkergal.fr>
2023-01-27 09:44:02 +0100

    feat(client): improve Container padding

--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -7,5 +7,7 @@ interface ContainerProps {
 }
 
 export default function Container({ children, className }: ContainerProps) {
-	return <div className={clsxMerge(className, 'h-full w-full p-4', 'lg:p-12')}>{children}</div>;
+	return (
+		<div className={clsxMerge(className, 'h-full w-full py-8 px-4', 'lg:p-12')}>{children}</div>
+	);
 }
