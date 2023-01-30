b1450ad0ca68e10a5ccd513589951f8af98e6a29
jkergal <hello@johannkergal.fr>
2023-01-30 23:49:32 +0100

    fix(server): -w mode issues, downgrades typescript to 4.8.4 + adds legacy watch mode to nodemon

--- server/src/resolvers/MemberResolver.ts
+++ server/src/resolvers/MemberResolver.ts
@@ -30,8 +30,8 @@ export default class MemberResolver {
 	}
 	@Mutation(() => Member)
 	async signUp(@Args() { username, email, password }: SignUpArgs): Promise<Member> {
+		console.log('RENEE');
 		const { user } = await MemberServices.signUp(username, email, password);
-
 		return user;
 	}
 
