36cbc75cb06b06b10ad4e643289f964cb1ead161
jkergal <hello@johannkergal.fr>
2023-02-02 17:13:43 +0100

    test(client): creates tests for getBrowserName + fixes getGrowser method

---
+++ client/src/constants/userAgents.ts
@@ -0,0 +1,18 @@
+export const USER_AGENTS = {
+	FIREFOX:
+        'Mozilla/5.0 (X11; Linux i686; rv:104.0) Gecko/20100101 Firefox/104.0',
+	SAMSUNG:
+		'Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-G955F Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/9.4 Chrome/67.0.3396.87 Mobile Safari/537.36',
+	OPERA:
+		'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 OPR/90.0.4480.54',
+	INTERNET_EXPLORER:
+		'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)',
+	EDGE_LEGACY:
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299',
+	EDGE_CHROMIUM:
+		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36 Edg/104.0.1293.70',
+	CHROME:
+		'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+	SAFARI:
+		'Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Mobile/15E148 Safari/604.1'
+};
