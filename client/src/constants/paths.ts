80bb1206dca6b1e72efa256982cccc8614b2a78e
NeelClaudel <neel.claudel@gmail.com>
2023-02-03 13:47:24 +0100

    fix: app and path

--- client/src/constants/paths.ts
+++ client/src/constants/paths.ts
@@ -1,5 +1,5 @@
 export const HOME_PATH = '/';
 export const SIGN_UP_PATH = '/sign-up';
 export const SIGN_IN_PATH = '/login';
-// Editor path
-export const EDITOR_PATH = '/editor';
+//Code Editor path
+export const CODE_EDITOR_PATH = '/code-editor';
