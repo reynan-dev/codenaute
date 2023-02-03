d82b58d41b92cdd758e6052eb26d7cf6903b40d2
reynan-dev <vieira@reynan.dev>
2023-02-03 10:37:44 +0100

    feat(server): including fileExplorer

--- client/src/pages/editor/editor.page.tsx
+++ client/src/pages/editor/editor.page.tsx
@@ -3,20 +3,26 @@ import {
 	SandpackConsole,
 	SandpackLayout,
 	SandpackPreview,
-	SandpackProvider
+	SandpackProvider,
+	SandpackFileExplorer
 } from '@codesandbox/sandpack-react';
 //import CustomSandpack from "components//CustomCodeEditor/CustomSandpack";
 
 export default function Editor() {
 	return (
-		<SandpackProvider theme={'dark'} style={{ height: '100%' }}>
+		<SandpackProvider theme={'dark'} style={{ height: '100%' }} template="react-ts">
+
 			<SandpackLayout
 				style={{
 					width: '100%',
 					height: '100%'
 				}}
-				draggable={true}
 			>
+				<SandpackFileExplorer
+ 				style={{
+					width: '10%',
+					height: '100%'
+				}}/>
 				<SandpackCodeEditor
 					showTabs={true}
 					showLineNumbers={true}
