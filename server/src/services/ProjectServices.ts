02b0815b9bd86d317fc09088c51b0b600f672ef9
reynan-dev <vieira@reynan.dev>
2023-02-03 15:12:32 +0100

    feat(server): update ProjectServices

--- server/src/services/ProjectServices.ts
+++ server/src/services/ProjectServices.ts
@@ -1,9 +1,38 @@
 import BaseServices from 'services/base/BaseServices';
 import Project from 'entities/Project';
+import { ErrorMessages } from 'utils/enums/ErrorMessages';
 
 class ProjectServices extends BaseServices {
 	constructor() {
 		super(Project);
+	}
+
+	async findAllPublic(): Promise<Project[]> {
+		return this.repository.find({ where: { isPublic: true } });
+	}
+
+	async findByMemberId(memberId: string): Promise<Project[]> {
+		return this.repository.find({ where: { members: { id: memberId } } });
+	}
+
+	async share(id: string, members: string[]): Promise<Project> {
+		const project = await this.repository.findOne(id);
+
+		if (!project) throw new Error(ErrorMessages.PROJECT_NOT_FOUND);
+
+		project.members = [...project.members, ...members];
+
+		return this.repository.save(project);
+	}
+
+	async addFile(id: string, files: string[]): Promise<Project> {
+		const project = await this.repository.findOne(id);
+
+		if (!project) throw new Error(ErrorMessages.PROJECT_NOT_FOUND);
+
+		project.files = [...project.files, ...files];
+
+		return this.repository.save(project);
 	}
 }
 
