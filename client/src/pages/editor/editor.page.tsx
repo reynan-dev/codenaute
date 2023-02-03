c1de0642477ec07ee2bf3f7d2fda66211cc55864
reynan-dev <vieira@reynan.dev>
2023-02-03 13:00:08 +0100

    feat(server): including files and dependencies

--- client/src/pages/editor/editor.page.tsx
+++ client/src/pages/editor/editor.page.tsx
@@ -6,11 +6,63 @@ import {
 	SandpackProvider,
 	SandpackFileExplorer
 } from '@codesandbox/sandpack-react';
-//import CustomSandpack from "components//CustomCodeEditor/CustomSandpack";
+import { findBreakingChanges } from 'graphql';
+import { isBooleanObject } from 'util/types';
+//import CustomSandpack from "components/CustomCodeEditor/CustomSandpack";
+
+const files: File[] = [
+	{
+		name: "index.ts",
+		code: "console.log('haha')"
+	},
+	{
+		name: "package.json",
+		code: `{
+		"dependencies": {
+		"react": "^18.0.0",
+		"react-dom": "^18.0.0",
+		"react-scripts": "^4.0.0"
+		},
+		"devDependencies": {
+		"@types/react": "^18.0.0",
+		"@types/react-dom": "^18.0.0",
+		"typescript": "^4.0.0",
+		"jest": "^27.0.0"
+		},
+		"main": "/index.tsx"
+		}`
+	}
+];
+
+const dependencies = JSON.parse(files.filter(e => e.name === 'package.json')[0].code).dependencies
+const devDependencies = JSON.parse(files.filter(e => e.name === 'package.json')[0].code).devDependencies
+
+interface File {
+	[k: string]: string;
+
+	name: string;
+	code: string;
+}
+
+const ArrayToObject = (files: File[]) => {
+
+	let filesObject: File = {name: '', code: ''};
+
+	files.map((e) => {
+		filesObject[e.name] = `${e.code}`
+	})
+
+	return filesObject
+}
 
 export default function Editor() {
 	return (
-		<SandpackProvider theme={'dark'} style={{ height: '100%' }} template="react-ts">
+		<SandpackProvider
+		theme={'dark'}
+		style={{ height: '100%' }}
+		files= { ArrayToObject(files) }
+		customSetup={{dependencies: dependencies, devDependencies: devDependencies}}
+		>
 
 			<SandpackLayout
 				style={{
