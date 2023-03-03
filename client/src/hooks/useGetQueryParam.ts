197ba2a1b9ad959664225660b8efee182d1f3a95
jkergal <hello@johannkergal.fr>
2023-03-03 12:27:49 +0100

    style(client): formats code

--- client/src/hooks/useGetQueryParam.ts
+++ client/src/hooks/useGetQueryParam.ts
@@ -3,8 +3,6 @@ import { useLocation } from 'react-router-dom';
 
 export const useGetQueryParam = (param: string) => {
 	const { search } = useLocation();
-
 	const query = React.useMemo(() => new URLSearchParams(search), [search]);
-
   return query.get(param)
 };
