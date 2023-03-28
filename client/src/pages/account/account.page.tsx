2ca8eeee8db0d14784f315903dd08c529c2cb9ed
Johann Kergal <89657288+jkergal@users.noreply.github.com>
2023-03-28 14:14:20 +0200

    feat(account): implements delete account section (#46)
    ## Ticket
    
    **Title: ETQ Member, j’ai une page pour supprimer mon compte**
    **Notion Link:
    https://www.notion.so/jkergal/ETQ-Member-j-ai-une-page-pour-supprimer-mon-compte-692140cd72e04c5689a1c49849c4d8de?pvs=4**
    
    ## Description
    - adds delete account section in /account
    - plugs it to server
    
    
    ![image](https://user-images.githubusercontent.com/89657288/222709186-13d770c5-65d8-4edb-96d6-d85cc43e4d91.png)
    
    
    ## Guidelines
    
    🔗 Link your PRs to their related Notion tickets
    ✍️ Use the commit convention to your pull request title
    📃 If your code affects the build, update `README.md`
    🚫 Do not merge a PR until all comments are resolved
    🗑 Remove your branch after merging

--- client/src/pages/account/account.page.tsx
+++ client/src/pages/account/account.page.tsx
@@ -1,15 +1,22 @@
 import Container from 'components/Container';
 import { FiUser } from 'react-icons/fi';
 import { MdPassword } from 'react-icons/md';
-import { PRIMARY } from 'styles/colors';
+import { FiTrash2 } from 'react-icons/fi';
+
+import { DANGER, PRIMARY } from 'styles/colors';
 import { twJoin } from 'tailwind-merge';
 
 interface AccountPageProps {
 	updateInformationsForm: JSX.Element;
 	updatePasswordForm: JSX.Element;
+	deleteAccountForm: JSX.Element;
 }
 
-export const AccountPage = ({ updateInformationsForm, updatePasswordForm }: AccountPageProps) => {
+export const AccountPage = ({
+	updateInformationsForm,
+	updatePasswordForm,
+	deleteAccountForm
+}: AccountPageProps) => {
 	return (
 		<Container className='flex items-center justify-center'>
 			<div
@@ -29,12 +36,19 @@ export const AccountPage = ({ updateInformationsForm, updatePasswordForm }: Acco
 					</div>
 					{updateInformationsForm}
 				</Container>
-				<Container className='py-14'>
+				<Container className='border-b border-dark-700 py-14'>
 					<div className='mb-5 flex items-center space-x-4'>
 						<MdPassword size={24} color={PRIMARY.DEFAULT} />
 						<h3>Change password</h3>
 					</div>
 					{updatePasswordForm}
+				</Container>
+				<Container className='py-14'>
+					<div className='mb-5 flex items-center space-x-4'>
+						<FiTrash2 size={24} color={DANGER.DEFAULT} className='mb-0.5' />
+						<h3>Delete account</h3>
+					</div>
+					{deleteAccountForm}
 				</Container>
 			</div>
 		</Container>
--- client/src/tests/helpers/areSameValues.test.ts
+++ client/src/tests/helpers/areSameValues.test.ts
@@ -3,28 +3,28 @@ import { areSameValues } from 'helpers/areSameValues';
 describe('areSameValues()', () => {
 	describe('When all values in the two objects ARE strictly equals', () => {
 		it('should return true', () => {
-			const obj1 = {email: "email@address.com", username: "username"};
-			const obj2 = {email: "email@address.com", username: "username"};
+			const obj1 = { email: 'email@address.com', username: 'username' };
+			const obj2 = { email: 'email@address.com', username: 'username' };
 
-			expect(areSameValues(obj1, obj2)).toEqual(true)
+			expect(areSameValues(obj1, obj2)).toEqual(true);
 		});
 	});
 
 	describe('When all values in the two objects ARE NOT strictly equals', () => {
 		it('should return true', () => {
-			const obj1 = {email: "email@address.com", username: "username"};
-			const obj2 = {email: "another.email@address.com", username: "another.username"};
+			const obj1 = { email: 'email@address.com', username: 'username' };
+			const obj2 = { email: 'another.email@address.com', username: 'another.username' };
 
-			expect(areSameValues(obj1, obj2)).toEqual(false)
+			expect(areSameValues(obj1, obj2)).toEqual(false);
 		});
 	});
 
-    describe('When two identic keys in the 2 objects contain different values', () => {
+	describe('When two identic keys in the 2 objects contain different values', () => {
 		it('should return true', () => {
-			const obj1 = {email: "email@address.com", username: "username"};
-			const obj2 = {email: "another.email@address.com", username: "username"};
+			const obj1 = { email: 'email@address.com', username: 'username' };
+			const obj2 = { email: 'another.email@address.com', username: 'username' };
 
-			expect(areSameValues(obj1, obj2)).toEqual(false)
+			expect(areSameValues(obj1, obj2)).toEqual(false);
 		});
 	});
 });
