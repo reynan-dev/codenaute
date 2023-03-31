89ef909425921646ffbd35a2561c40f1670706b2
jkergal <hello@johannkergal.fr>
2023-03-31 12:20:31 +0200

    feat(client): renders navbar only if authenticated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,25 @@ import CrossDeviceBackground from 'components/CrossDeviceBackground';
 import Main from 'components/Main';
 import NavBar from 'components/NavBar';
 import { ACCOUNT_PATH, HOME_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from 'constants/paths';
+import AuthContext from 'context/auth.context';
 import { Account } from 'pages/account';
 import { Home } from 'pages/home';
 import { SignIn } from 'pages/signIn';
 import { SignUp } from 'pages/signUp';
+import { useContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/toastify.css';
 
 const App = () => {
+	const { isAuthenticated } = useContext(AuthContext);
+
 	return (
 		<>
 			<CrossDeviceBackground />
-			<div className='flex flex-col md:flex-row md:h-full'>
-
-				<NavBar className='fixed z-50' />
+			<div className='flex flex-col md:h-full md:flex-row'>
+				{isAuthenticated && <NavBar className='fixed z-50' />}
 				<Main>
 					<Routes>
 						<Route path={HOME_PATH} element={<Home />} />
