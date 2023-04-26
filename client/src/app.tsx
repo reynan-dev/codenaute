6bb43ae321a694fc7cbdfc0202c8be412a678602
jkergal <hello@johannkergal.fr>
2023-04-26 12:01:39 +0200

    feat(front): re updates app.tsx codes

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,61 +1,60 @@
 import ConditionalRoute from 'components/conditionnal-route';
 import CrossDeviceBackground from 'components/cross-device-background';
-import { HOME_PATH, SIGN_UP_PATH, SIGN_IN_PATH, ACCOUNT_PATH, CODE_PATH } from 'constants/paths';
+import Main from 'components/main';
+import NavBar from 'components/nav-bar';
+import { ACCOUNT_PATH, HOME_PATH, SIGN_IN_PATH, SIGN_UP_PATH } from 'constants/paths';
+import AuthContext from 'context/auth.context';
 import { Account } from 'pages/account';
-import { Code } from 'pages/code';
 import { Home } from 'pages/home';
 import { SignIn } from 'pages/sign-in';
 import { SignUp } from 'pages/sign-up';
-import { Routes, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/toastify.css';
 
 const App = () => {
+	const { isAuthenticated } = useContext(AuthContext);
+
 	return (
 		<>
 			<CrossDeviceBackground />
-			<div className='flex flex-col lg:h-full'>
-				<Routes>
-					<Route path={HOME_PATH} element={<Home />} />
+			<div className='flex flex-col md:h-full md:flex-row'>
+				{isAuthenticated && <NavBar className='fixed z-50' />}
+				<Main>
+					<Routes>
+						<Route path={HOME_PATH} element={<Home />} />
 
-					<Route
-						path={SIGN_UP_PATH}
-						element={
-							<ConditionalRoute type='unauth'>
-								<SignUp />
-							</ConditionalRoute>
-						}
-					/>
+						<Route
+							path={SIGN_UP_PATH}
+							element={
+								<ConditionalRoute type='unauth'>
+									<SignUp />
+								</ConditionalRoute>
+							}
+						/>
 
-					<Route
-						path={SIGN_IN_PATH}
-						element={
-							<ConditionalRoute type='unauth'>
-								<SignIn />
-							</ConditionalRoute>
-						}
-					/>
+						<Route
+							path={SIGN_IN_PATH}
+							element={
+								<ConditionalRoute type='unauth'>
+									<SignIn />
+								</ConditionalRoute>
+							}
+						/>
 
-					<Route
-						path={ACCOUNT_PATH}
-						element={
-							<ConditionalRoute type='private'>
-								<Account />
-							</ConditionalRoute>
-						}
-					/>
-
-					<Route
-						path={CODE_PATH}
-						element={
-							<ConditionalRoute type='private'>
-								<Code />
-							</ConditionalRoute>
-						}
-					/>
-				</Routes>
-				<ToastContainer theme='colored' />
+						<Route
+							path={ACCOUNT_PATH}
+							element={
+								<ConditionalRoute type='private'>
+									<Account />
+								</ConditionalRoute>
+							}
+						/>
+					</Routes>
+					<ToastContainer theme='colored' />
+				</Main>
 			</div>
 		</>
 	);
