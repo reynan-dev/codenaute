219b32f855aa6a244dc9bc2609605fb382ef8a54
jkergal <hello@johannkergal.fr>
2023-04-26 12:54:03 +0200

    feat(fornt): setup sandpack + code page and route

--- client/src/pages/code/code.page.tsx
+++ client/src/pages/code/code.page.tsx
@@ -1,3 +1,112 @@
+// import { findBreakingChanges } from 'graphql';
+
+import {
+	SandpackCodeEditor,
+	SandpackConsole,
+	SandpackFileExplorer,
+	SandpackLayout,
+	SandpackPreview,
+	SandpackProvider
+} from '@codesandbox/sandpack-react';
+
+// import { isBooleanObject } from 'util/types';
+const files: File[] = [
+	{
+		name: 'index.ts',
+		code: "console.log('haha')"
+	},
+	{
+		name: 'package.json',
+		code: `{
+		"dependencies": {
+		"react": "^18.0.0",
+		"react-dom": "^18.0.0",
+		"react-scripts": "^4.0.0"
+		},
+		"devDependencies": {
+		"@types/react": "^18.0.0",
+		"@types/react-dom": "^18.0.0",
+		"typescript": "^4.0.0",
+		"jest": "^27.0.0"
+		},
+		"main": "/index.tsx"
+		}`
+	}
+];
+
+const dependencies = JSON.parse(
+	files.filter((e) => e.name === 'package.json')[0].code
+).dependencies;
+
+const devDependencies = JSON.parse(
+	files.filter((e) => e.name === 'package.json')[0].code
+).devDependencies;
+
+interface File {
+	[k: string]: string;
+
+	name: string;
+	code: string;
+}
+
+const ArrayToObject = (files: File[]) => {
+	let filesObject: File = { name: '', code: '' };
+
+	files.map((e) => {
+		return (filesObject[e.name] = `${e.code}`);
+	});
+
+	return filesObject;
+};
+
 export const CodePage = () => {
-	return <div className='flex px-5 py-3'>CODE EDITOR</div>;
+	return (
+		<SandpackProvider
+			theme={'dark'}
+			style={{ height: '100%' }}
+			files={ArrayToObject(files)}
+			customSetup={{ dependencies: dependencies, devDependencies: devDependencies }}
+		>
+			<SandpackLayout
+				style={{
+					width: '100%',
+					height: '100%'
+				}}
+			>
+				<SandpackFileExplorer
+					style={{
+						width: '10%',
+						height: '100%'
+					}}
+				/>
+				<SandpackCodeEditor
+					showTabs={true}
+					showLineNumbers={true}
+					showInlineErrors={true}
+					wrapContent={true}
+					closableTabs
+					showRunButton={true}
+					style={{
+						width: '100%',
+						height: '100%'
+					}}
+				/>
+				<SandpackPreview
+					showNavigator
+					showOpenInCodeSandbox={false}
+					style={{
+						width: '100%',
+						height: '100%'
+					}}
+				/>
+				<SandpackConsole
+					style={{
+						width: '100%',
+						height: '100%'
+					}}
+				/>
+			</SandpackLayout>
+			{/* <CustomSandpack /> */}
+		</SandpackProvider>
+	);
 };
