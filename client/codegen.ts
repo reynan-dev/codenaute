9664890923a8766fae6a0a89e7cf0f2a36f075b0
reynan-dev <vieira@reynan.dev>
2023-05-01 14:35:53 +0200

    fix(client): updating schema uri

--- client/codegen.ts
+++ client/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
 	overwrite: true,
-	schema: 'http://localhost:5000',
+	schema: 'http://server:5000',
 	documents: 'src/graphql/**/*.ts',
 	generates: {
 		'src/graphql/__generated__/': {
