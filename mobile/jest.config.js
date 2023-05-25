114a424cca53d9a031236da09a0e0de9eb629fb9
reynan-dev <vieira@reynan.dev>
2023-05-25 11:07:08 +0200

    test(mobile): including jest config and coverage commands

---
+++ mobile/jest.config.js
@@ -0,0 +1,35 @@
+'use strict';
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+Object.defineProperty(exports, '__esModule', { value: true });
+exports.default = {
+	// Automatically clear mock calls, instances, contexts and results before every test
+	clearMocks: true,
+	// Indicates whether the coverage information should be collected while executing the test
+	// collectCoverage: true,
+	// An array of glob patterns indicating a set of files for which coverage information should be collected
+	collectCoverageFrom: ['src/**/*.tsx'],
+	coverageThreshold: {
+		global: {
+			lines: 90,
+			statements: 90
+		}
+	},
+	// The directory where Jest should output its coverage files
+	coverageDirectory: './tests/coverage',
+	// An array of regexp pattern strings used to skip coverage collection
+	coveragePathIgnorePatterns: ['/node_modules/'],
+	// Indicates which provider should be used to instrument code for coverage
+	coverageProvider: 'v8',
+	// A preset that is used as a base for Jest's configuration
+	preset: '@testing-library/react-native',
+	// The test environment that will be used for testing
+	moduleDirectories: ['node_modules', 'src'],
+	// A list of files that Jest should use to before all test suites
+	setupFilesAfterEnv: [
+		'<rootDir>/tests/setup.jest.ts',
+		'@testing-library/jest-native/extend-expect'
+	]
+};
