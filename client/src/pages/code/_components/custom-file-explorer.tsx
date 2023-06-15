ed901ec82f13c22cc91bf8ba4c74c812b2e87e9c
jkergal <hello@johannkergal.fr>
2023-06-15 23:43:56 +0200

    feat(custom-file-explorer): improves interactions

--- client/src/pages/code/_components/custom-file-explorer.tsx
+++ client/src/pages/code/_components/custom-file-explorer.tsx
@@ -10,62 +10,95 @@ interface CustomFileExplorerProps {
 	files: SandpackFiles | null;
 }
 
-function getFilePaths(
-	node: TreeNode,
-	fileName: string,
-	currentPath = '',
-	paths: string[] = []
-): string[] {
-	const newPath = currentPath === '' ? node.name : `${currentPath}/${node.name}`;
+// function getFilePaths(
+// 	node: TreeNode,
+// 	fileName: string,
+// 	currentPath = '',
+// 	paths: string[] = []
+// ): string[] {
+// 	const newPath = currentPath === '' ? node.name : `${currentPath}/${node.name}`;
 
-	if (node.name === fileName) {
-		paths.push(newPath);
-	}
+// 	if (node.name === fileName) {
+// 		paths.push(newPath);
+// 	}
 
-	if (node.children) {
-		for (const child of node.children) {
-			getFilePaths(child, fileName, newPath, paths);
-		}
-	}
+// 	if (node.children) {
+// 		for (const child of node.children) {
+// 			getFilePaths(child, fileName, newPath, paths);
+// 		}
+// 	}
 
-	return paths;
-}
+// 	return paths;
+// }
 
 export const CustomFileExplorer = ({ className, files }: CustomFileExplorerProps) => {
 	const { sandpack } = useSandpack();
+	const [expandedNodes, setExpandedNodes] = useState<string[]>([]);
 
-	// console.log(sandpack.visibleFiles)
+	console.log(sandpack.visibleFiles);
 
 	const filePaths = files !== null ? Object.keys(files) : [];
 	const filesTree = buildProjectTree(filePaths);
 
+	// const hasVisibleFile = (node: TreeNode) => {
+	// 	console.log('check visible');
+	// 	return node.children
+	// 		?.map((child) => sandpack.visibleFiles.includes(child.path))
+	// 		.some((childHasVisibleFile) => childHasVisibleFile === true);
+	// };
+
+	// const hasVisibleChildren = (node: TreeNode): boolean => {
+	// 	if (node.children) {
+	// 		for (const child of node.children) {
+	// 			if (sandpack.visibleFiles.includes(child.path)) {
+	// 				return true;
+	// 			}
+
+	// 			if (hasVisibleChildren(child)) {
+	// 				return true;
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return false;
+	// };
+
+	// const isFileVisible = (filePath: string) => {
+	// 	return sandpack.visibleFiles.includes(filePath)
+	// }
+
 	const style = { icons: 'mr-2' };
-
-	const handleFileClick = (node: TreeNode) => {
-		if (node.children !== undefined && node.children.length > 0) return;
-		sandpack.openFile(node.path);
-	};
-
-	const hasVisibleFile = (node: TreeNode) => {
-		console.log('check visible');
-		return node.children
-			?.map((child) => sandpack.visibleFiles.includes(child.path))
-			.some((childHasVisibleFile) => childHasVisibleFile === true);
-	};
 
 	// useEffect(() => {
 	// 	setVisibleFiles(sandpack.visibleFiles)
 	// }, [sandpack.visibleFiles])
 
 	const renderNode = (node: TreeNode) => {
+		const isExpanded = expandedNodes.includes(node.path);
+
+		const toggleNode = (node: TreeNode) => {
+			if (isExpanded) {
+				setExpandedNodes(expandedNodes.filter((path) => path !== node.path));
+			} else {
+				setExpandedNodes((prevExpandedNodes) => [...prevExpandedNodes, node.path]);
+			}
+		};
+
+		const handleFileClick = (node: TreeNode, event: React.MouseEvent) => {
+			event.stopPropagation();
+			console.log({ node });
+			if (node.children !== undefined && node.children.length > 0) return toggleNode(node);
+			sandpack.openFile(node.path);
+		};
+
 		return (
 			<React.Fragment key={node.name}>
 				<button
 					className='flex items-center py-1 hover:text-primary-200'
-					onClick={() => handleFileClick(node)}
+					onClick={(event) => handleFileClick(node, event)}
 				>
 					{node.children && node.children.length > 0 ? (
-						hasVisibleFile(node) ? (
+						isExpanded ? (
 							<AiFillFolderOpen className={style.icons} />
 						) : (
 							<AiFillFolder className={style.icons} />
@@ -75,9 +108,15 @@ export const CustomFileExplorer = ({ className, files }: CustomFileExplorerProps
 					)}
 					<span>{node.name}</span>
 				</button>
-				{node.children &&
+
+				{isExpanded &&
+					node.children &&
 					node.children.map((child) => (
-						<button className='ml-4' key={child.name} onClick={() => handleFileClick(node)}>
+						<button
+							className='ml-4'
+							key={child.name}
+							onClick={(event) => handleFileClick(node, event)}
+						>
 							{renderNode(child)}
 						</button>
 					))}
