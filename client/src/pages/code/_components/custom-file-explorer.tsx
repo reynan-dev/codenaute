aba40b227ab24936724e605ed19e1e16fbf40c95
jkergal <hello@johannkergal.fr>
2023-06-15 23:45:31 +0200

    refactor(custom-file-explorer): deletes useless code

--- client/src/pages/code/_components/custom-file-explorer.tsx
+++ client/src/pages/code/_components/custom-file-explorer.tsx
@@ -1,7 +1,6 @@
 import { SandpackFiles, useSandpack } from '@codesandbox/sandpack-react';
 import { TreeNode, buildProjectTree } from 'helpers/format-file-path';
-import React, { useEffect, useState } from 'react';
-// import { formatFilePath } from 'helpers/format-file-path';
+import React, { useState } from 'react';
 import { AiFillFolder, AiFillFolderOpen, AiOutlineFile } from 'react-icons/ai';
 import { twMerge } from 'tailwind-merge';
 
@@ -10,68 +9,14 @@ interface CustomFileExplorerProps {
 	files: SandpackFiles | null;
 }
 
-// function getFilePaths(
-// 	node: TreeNode,
-// 	fileName: string,
-// 	currentPath = '',
-// 	paths: string[] = []
-// ): string[] {
-// 	const newPath = currentPath === '' ? node.name : `${currentPath}/${node.name}`;
-
-// 	if (node.name === fileName) {
-// 		paths.push(newPath);
-// 	}
-
-// 	if (node.children) {
-// 		for (const child of node.children) {
-// 			getFilePaths(child, fileName, newPath, paths);
-// 		}
-// 	}
-
-// 	return paths;
-// }
-
 export const CustomFileExplorer = ({ className, files }: CustomFileExplorerProps) => {
 	const { sandpack } = useSandpack();
 	const [expandedNodes, setExpandedNodes] = useState<string[]>([]);
 
-	console.log(sandpack.visibleFiles);
-
 	const filePaths = files !== null ? Object.keys(files) : [];
 	const filesTree = buildProjectTree(filePaths);
 
-	// const hasVisibleFile = (node: TreeNode) => {
-	// 	console.log('check visible');
-	// 	return node.children
-	// 		?.map((child) => sandpack.visibleFiles.includes(child.path))
-	// 		.some((childHasVisibleFile) => childHasVisibleFile === true);
-	// };
-
-	// const hasVisibleChildren = (node: TreeNode): boolean => {
-	// 	if (node.children) {
-	// 		for (const child of node.children) {
-	// 			if (sandpack.visibleFiles.includes(child.path)) {
-	// 				return true;
-	// 			}
-
-	// 			if (hasVisibleChildren(child)) {
-	// 				return true;
-	// 			}
-	// 		}
-	// 	}
-
-	// 	return false;
-	// };
-
-	// const isFileVisible = (filePath: string) => {
-	// 	return sandpack.visibleFiles.includes(filePath)
-	// }
-
 	const style = { icons: 'mr-2' };
-
-	// useEffect(() => {
-	// 	setVisibleFiles(sandpack.visibleFiles)
-	// }, [sandpack.visibleFiles])
 
 	const renderNode = (node: TreeNode) => {
 		const isExpanded = expandedNodes.includes(node.path);
@@ -86,7 +31,6 @@ export const CustomFileExplorer = ({ className, files }: CustomFileExplorerProps
 
 		const handleFileClick = (node: TreeNode, event: React.MouseEvent) => {
 			event.stopPropagation();
-			console.log({ node });
 			if (node.children !== undefined && node.children.length > 0) return toggleNode(node);
 			sandpack.openFile(node.path);
 		};
