95b20e670b0df785c95353e1b5322334cfed864e
jkergal <hello@johannkergal.fr>
2023-06-21 10:45:55 +0200

    feat(custom-file-explorer): improves context-menu style

--- client/src/pages/code/_components/context-menu.tsx
+++ client/src/pages/code/_components/context-menu.tsx
@@ -15,11 +15,20 @@ export const ContextMenu: React.FC<ContextMenuProps> = ({ position, onClose, onA
 	};
 
 	return (
-		<div className='fixed z-50 bg-white shadow-md' style={{ left: x, top: y }}>
-			<ul>
-				<li onClick={() => handleAction('open')}>Open</li>
-				<li onClick={() => handleAction('rename')}>Rename</li>
-				<li onClick={() => handleAction('delete')}>Delete</li>
+		<div
+			className='fixed z-50 w-36 rounded-sm bg-dark-800 text-white shadow-lg shadow-black'
+			style={{ left: x, top: y }}
+		>
+			<ul className='py-2'>
+				<li className='px-3 py-1 hover:bg-dark-600' onClick={() => handleAction('open')}>
+					Open
+				</li>
+				<li className='px-3 py-1 hover:bg-dark-600' onClick={() => handleAction('rename')}>
+					Rename
+				</li>
+				<li className='px-3 py-1 hover:bg-dark-600' onClick={() => handleAction('delete')}>
+					Delete
+				</li>
 			</ul>
 		</div>
 	);
