6a7b54d9bb622f7c9847d22672ab8f2751eaa5c2
Reynan VIEIRA <vieira@reynan.dev>
2023-06-23 10:59:14 +0200

    refator(MemberServices): removed useless method and Error responsability

--- server/src/services/MemberServices.ts
+++ server/src/services/MemberServices.ts
@@ -37,23 +37,6 @@ export class MemberServices extends BaseServices {
 		return await this.SessionServices.delete(token);
 	}
 
-	async findOneBySessionToken(token: string): Promise<Member | null> {
-		const member = await this.repository.findOne({
-			where: { sessions: { token } },
-			relations: [
-				'ownedProjects',
-				'projectsInvitedOn',
-				'favoritedProjects',
-				'followers',
-				'following'
-			]
-		});
-
-		if (!member) return null;
-
-		return member;
-	}
-
 	async findOneById(memberId: UUID): Promise<Member | null> {
 		const member = await this.repository.findOne({
 			where: { id: memberId },
@@ -88,15 +71,7 @@ export class MemberServices extends BaseServices {
 		return member;
 	}
 
-	async followMember(memberId: UUID, memberToFollowId: UUID) {
-		const member = (await this.findOneById(memberId)) as Member;
-		const memberToFollow = (await this.findOneById(memberToFollowId)) as Member;
-
-		if (!member || !memberToFollow) throw Error(ErrorMessages.MEMBER_NOT_FOUND);
-
-		if (member.following.includes(memberToFollow))
-			throw Error(ErrorMessages.ALREADY_FOLLOWING_MEMBER_ERROR_MESSAGE);
-
+	async followMember(member: Member, memberToFollow: Member) {
 		member.following.push(memberToFollow);
 
 		return this.repository.save(member);
@@ -116,26 +91,14 @@ export class MemberServices extends BaseServices {
 		return await this.update(member.id, { isValidEmail: true });
 	}
 
-	async updatePassword(email: string, newPassword: string, confirmedNewPassword: string) {
-		const user = (await this.findOneBy({ email })) as Member;
-
-		if (!user) throw Error(ErrorMessages.INVALID_EMAIL_ERROR_MESSAGE);
-
-		if (newPassword !== confirmedNewPassword)
-			throw Error(ErrorMessages.PASSWORDS_DO_NOT_MATCH_ERROR_MESSAGE);
-
-		const hashedPassword = hashSync(newPassword, 10);
+	async updatePassword(user: Member, password: string) {
+		const hashedPassword = hashSync(password, 10);
 
 		return await this.update(user.id, { hashedPassword });
 	}
 
-	async deleteAccount(memberId: UUID, password: string) {
-		const user = (await this.findOneBy({ id: memberId })) as Member;
-
-		if (!compareSync(password, user.hashedPassword))
-			throw Error(ErrorMessages.INVALID_PASSWORD_ERROR_MESSAGE);
-
-		await this.delete(memberId);
+	async deleteAccount(member: Member) {
+		await this.delete(member.id);
 
 		return true;
 	}
