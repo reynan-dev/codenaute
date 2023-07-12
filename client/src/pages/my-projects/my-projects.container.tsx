e660dba2aaffd00cdff40cb1f9e124099a02683e
jkergal <hello@johannkergal.fr>
2023-07-12 22:52:26 +0200

    feat(front): init my projects route

---
+++ client/src/pages/my-projects/my-projects.container.tsx
@@ -0,0 +1,5 @@
+import { MyProjectsPage } from 'pages/my-projects/my-projects.page';
+
+export const MyProjectsContainer = () => {
+	return <MyProjectsPage />;
+};
---
+++ client/src/pages/my-projects/my-projects.page.tsx
@@ -0,0 +1,5 @@
+import Container from 'components/container';
+
+export const MyProjectsPage = () => {
+	return <Container className='flex items-center justify-center'>prout</Container>;
+};
