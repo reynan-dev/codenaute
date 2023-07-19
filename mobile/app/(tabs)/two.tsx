85e4647fae4a59c1da0cebbc56666834a8ce32a1
jkergal <hello@johannkergal.fr>
2023-07-19 21:38:32 +0200

    chore(mobile): install tailwind-merge

--- mobile/app/(tabs)/two.tsx
+++ mobile/app/(tabs)/two.tsx
@@ -2,11 +2,12 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { twMerge } from 'tailwind-merge';
 
 export default function TabTwoScreen() {
 	return (
 		<View style={styles.container}>
-			<Text className='text-yellow-500'>Tab Two</Text>
+			<Text className={twMerge('text-yellow-500', 'font-bold')}>Tab Two</Text>
 			<View style={styles.separator} lightColor='#eee' darkColor='rgba(255,255,255,0.1)' />
 			<EditScreenInfo path='app/(tabs)/two.tsx' />
 		</View>
