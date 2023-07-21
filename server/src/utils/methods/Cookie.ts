d9679101a0e634acd63c583bf5aa6f093bb9cec0
Reynan VIEIRA <vieira@reynan.dev>
2023-07-21 14:18:01 +0200

    refactor(server): refactored Cookie class, to update set and get session methods.

--- server/src/utils/methods/Cookie.ts
+++ server/src/utils/methods/Cookie.ts
@@ -1,24 +1,27 @@
 import { ExpressContext } from 'apollo-server-express';
-import { parse } from 'cookie';
 import { Validations } from 'utils/enums/Validations';
 
 export abstract class Cookie {
-	public static getSessionToken = (ctx: ExpressContext): string | undefined => {
-		const rawCookies = ctx.req.headers.cookie;
+	private static _cookieOptions(token: string) {
+		return {
+			validation: 1000 * 60 * 60 * 24 * Validations.MAX_AGE_DAYS,
+			token: token
+		};
+	}
+
+	public static getSessionToken(ctx: ExpressContext): string | undefined {
+		const rawCookies = ctx.req.headers.authorization;
 
 		if (!rawCookies) return undefined;
 
-		const cookies = parse(rawCookies);
+		const cookies = JSON.parse(rawCookies);
 
-		return cookies.session;
-	};
+		if (cookies.validation < Date.now()) return undefined;
 
-	public static setSessionToken = (ctx: ExpressContext, token: string) => {
-		ctx.res.cookie('session', token, {
-			httpOnly: true,
-			secure: true,
-			sameSite: true,
-			maxAge: 1000 * 60 * 60 * 24 * Validations.MAX_AGE_DAYS
-		});
-	};
+		return cookies.token;
+	}
+
+	public static setSessionToken(token: string) {
+		return JSON.stringify(this._cookieOptions(token));
+	}
 }
