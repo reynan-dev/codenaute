632261afb4ec3420e12d5a2d083b66e7f614ccd4
Reynan VIEIRA <vieira@reynan.dev>
2023-07-21 14:44:25 +0200

    refactor(Cookies): updating Cookie class to split responsabilities

--- server/src/utils/methods/Cookie.ts
+++ server/src/utils/methods/Cookie.ts
@@ -1,27 +1,32 @@
 import { ExpressContext } from 'apollo-server-express';
+import { Session } from 'models/Session';
 import { Validations } from 'utils/enums/Validations';
 
 export abstract class Cookie {
-	private static _cookieOptions(token: string) {
+	private static _cookieOptions(session: Session) {
 		return {
-			validation: 1000 * 60 * 60 * 24 * Validations.MAX_AGE_DAYS,
-			token: token
+			validation: Date.now() + 1000 * 60 * 60 * 24 * Validations.MAX_AGE_DAYS,
+			token: session.token
 		};
+	}
+
+	private static _rawCookieValidation(rawCookies: string) {
+		if (!rawCookies) return undefined;
+
+		const cookies = JSON.parse(rawCookies);
+
+		return cookies.validation < Date.now() ? undefined : cookies;
 	}
 
 	public static getSessionToken(ctx: ExpressContext): string | undefined {
 		const rawCookies = ctx.req.headers.authorization;
 
-		if (!rawCookies) return undefined;
-
-		const cookies = JSON.parse(rawCookies);
-
-		if (cookies.validation < Date.now()) return undefined;
+		const cookies = this._rawCookieValidation(rawCookies as string);
 
 		return cookies.token;
 	}
 
-	public static setSessionToken(token: string) {
-		return JSON.stringify(this._cookieOptions(token));
+	public static setSessionToken(session: Session) {
+		return JSON.stringify(this._cookieOptions(session));
 	}
 }
