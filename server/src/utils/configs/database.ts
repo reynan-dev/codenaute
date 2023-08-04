6ab7b73cf9d1a6234bf4ebe9d7c0a97b6fb1ea71
Reynan VIEIRA <vieira@reynan.dev>
2023-08-04 12:57:00 +0200

    refactor(server): update Database config to include migrations config

--- server/src/utils/configs/database.ts
+++ server/src/utils/configs/database.ts
@@ -69,19 +69,19 @@ export abstract class Database {
 		`${__dirname}/../../**/models/*.${process.env.NODE_ENV === Environment.IS_TEST ? 'ts' : 'js'}`
 	];
 	private static readonly migrations = [
-		`${__dirname}/../../**/migrations/*.${
-			process.env.NODE_ENV === Environment.IS_TEST ? 'ts' : 'js'
-		}`
+		`${__dirname}/../**/migrations/*.${process.env.NODE_ENV === Environment.IS_TEST ? 'ts' : 'js'}`
 	];
+	private static readonly migrationsRun = true;
 	private static readonly logging: LoggerOptions | undefined =
 		process.env.NODE_ENV === Environment.IS_PRODUCTION ? ['error'] : ['query', 'error'];
-	private static readonly synchronize: boolean = true;
+	private static readonly synchronize: boolean = false;
 
 	private static _dataSource: DataSource = new DataSource({
 		type: this.type,
 		url: this.url,
 		entities: this.models,
 		migrations: this.migrations,
+		migrationsRun: this.migrationsRun,
 		logging: this.logging,
 		synchronize: this.synchronize
 	});
@@ -134,4 +134,10 @@ export abstract class Database {
 			await this._dataSource.destroy();
 		}
 	}
+
+	static async dataSource() {
+		return this._dataSource;
+	}
 }
+
+export default Database.dataSource();
