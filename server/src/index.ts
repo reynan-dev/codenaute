f6b4a437e88d2a901fcc2c631342d4e49128b3e5
Reynan Vieira <vieira@reynan.dev>
2023-08-09 15:34:37 +0200

    chore(server): removing routing token

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,7 @@ import { Server } from 'utils/configs/server';
 
 import { MemberResolver } from 'resolvers/MemberResolver';
 import { ProjectResolver } from 'resolvers/ProjectResolver';
-import { RoutingTokenResolver } from 'resolvers/RoutingTokenResolver';
 
-Server.includeResolvers([MemberResolver, ProjectResolver, RoutingTokenResolver]);
+Server.includeResolvers([MemberResolver, ProjectResolver]);
 
 Server.start();
