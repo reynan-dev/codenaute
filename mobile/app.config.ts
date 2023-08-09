99657d42e8731a9ef87938fcad7f1ef5b4a6751f
jkergal <hello@johannkergal.fr>
2023-08-09 12:47:05 +0200

    feat(mobile): updates app logo

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -11,7 +11,7 @@ const config: ExpoConfig = {
 	version: '1.0.0',
 	orientation: 'portrait',
 	icon: './src/assets/icon.png',
-	userInterfaceStyle: 'light',
+	userInterfaceStyle: 'dark',
 	splash: {
 		image: './src/assets/splash.png',
 		resizeMode: 'contain',
